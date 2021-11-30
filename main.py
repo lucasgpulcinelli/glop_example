ede1cb084e9279016e096896593a1e0f4e05763a
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2021-11-30 15:17:11 -0300

    inital code cleanup

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
+#!/usr/bin/env python3
+
 from ortools.linear_solver import pywraplp
 
 
 def LinearProgrammingExample():
-    """Linear programming sample."""
-    # Instantiate a Glop solver, naming it LinearExample.
+    # Instantiate a Glop solver
     solver = pywraplp.Solver.CreateSolver('GLOP')
 
-    # Create the two variables and let them take on any non-negative value.
+    # Create the two variables and let them take on any non-negative value
     x = solver.NumVar(0, solver.infinity(), 'x')
     y = solver.NumVar(0, solver.infinity(), 'y')
 
-    print('Number of variables =', solver.NumVariables())
-
-    # Constraint 0: x + 2y <= 14.
+    # Add the constrains
     solver.Add(x + 2 * y <= 14.0)
-
-    # Constraint 1: 3x - y >= 0.
     solver.Add(3 * x - y >= 0.0)
-
-    # Constraint 2: x - y <= 2.
     solver.Add(x - y <= 2.0)
 
-    print('Number of constraints =', solver.NumConstraints())
 
-    # Objective function: 3x + 4y.
+    # Objective function: 3x + 4y
     solver.Maximize(3 * x + 4 * y)
 
-    # Solve the system.
+    # Solve the system
     status = solver.Solve()
 
     if status == pywraplp.Solver.OPTIMAL:
@@ -37,10 +31,7 @@ def LinearProgrammingExample():
     else:
         print('The problem does not have an optimal solution.')
 
-    print('\nAdvanced usage:')
-    print('Problem solved in %f milliseconds' % solver.wall_time())
-    print('Problem solved in %d iterations' % solver.iterations())
 
-
-LinearProgrammingExample()
+if __name__ == "__main__":
+    LinearProgrammingExample()
 
