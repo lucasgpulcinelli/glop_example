2a2ddaea03f32f74e47ae1d395a3311ef32901b9
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2021-12-07 11:29:35 -0300

    Add apache comments and explicit modifications

--- generic/generic.py
+++ generic/generic.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
+# Copyright 2010-2021 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file was changed by Lucas Eduardo Gulka Pulcinelli
+# to include a seaborn plot of the problem and modify the logic 
+# for ease of understanding.
+# Copyright 2021 Lucas Eduardo Gulka Pulcinelli
 
 from ortools.linear_solver import pywraplp
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-#TODO: make functions more generic?
 
 sns.set_style("whitegrid", {"grid.color": ".6", "grid.linestyle": ":"})
 
--- stigler/stigler.py
+++ stigler/stigler.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# Copyright 2010-2021 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file was changed by Lucas Eduardo Gulka Pulcinelli
+# to separate the data and nutrients from the python program to
+# a pandas dataframe
+# Copyright 2021 Lucas Eduardo Gulka Pulcinelli
+
 from ortools.linear_solver import pywraplp
 import pandas as pd
 
